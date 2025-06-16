869859b50178236de1c69210ff98ffd3f88a82da
laurapena290805 <penaloza.laura@correounivalle.edu.co>
2025-06-16 13:07:29 -0500

    Spring Boot 2 - Servicio Rest pais

---
+++ Spring Boot 2 - Servicio REST/pais/src/main/java/com/serviciciorest/pais/PaisApplication.java
@@ -0,0 +1,13 @@
+package com.serviciciorest.pais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaisApplication.class, args);
+	}
+
+}
---
+++ Spring Boot 2 - Servicio REST/pais/src/test/java/com/serviciciorest/pais/PaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviciciorest.pais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
