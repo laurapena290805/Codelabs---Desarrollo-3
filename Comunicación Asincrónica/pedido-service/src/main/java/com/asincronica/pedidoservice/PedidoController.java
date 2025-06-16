597dab2e4a09ca96b6686ff11662cc7ce2422c5b
laurapena290805 <penaloza.laura@correounivalle.edu.co>
2025-06-16 12:49:17 -0500

    Comunicación entre microservicios: Asincrónica

---
+++ Comunicación Asincrónica/pedido-service/src/main/java/com/asincronica/pedidoservice/PedidoController.java
@@ -0,0 +1,13 @@
+package com.asincronica.pedidoservice;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/pedidos")
+public class PedidoController {
+
+    @GetMapping("/crear")
+    public String crearPedido() {
+        return "El pedido está en proceso y se recibirá en cuanto llegue el mensaje asíncrono.";
+    }
+}
---
+++ Comunicación Asincrónica/producto-service/src/main/java/com/asincronica/productoservice/ProductoController.java
@@ -0,0 +1,20 @@
+package com.asincronica.productoservice;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/productos")
+public class ProductoController {
+
+    private final ProductorService productorService;
+
+    public ProductoController(ProductorService productorService) {
+        this.productorService = productorService;
+    }
+
+    @PostMapping("/enviar")
+    public String enviarProductos(@RequestBody List<ProductoDTO> productos) {
+        productorService.enviarListaProductos(productos);
+        return "Productos enviados con éxito al pedido-service";
+    }
+}
