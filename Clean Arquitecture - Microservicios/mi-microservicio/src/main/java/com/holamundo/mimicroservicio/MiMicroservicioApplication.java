07de3540caaf662619594d8ce230aafc5bf6e6ed
laurapena290805 <penaloza.laura@correounivalle.edu.co>
2025-06-16 11:31:41 -0500

    Clean Architecture en Microservicios

---
+++ Clean Arquitecture - Microservicios/mi-microservicio/src/main/java/com/holamundo/mimicroservicio/MiMicroservicioApplication.java
@@ -0,0 +1,13 @@
+package com.holamundo.mimicroservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiMicroservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiMicroservicioApplication.class, args);
+	}
+
+}
---
+++ Clean Arquitecture - Microservicios/mi-microservicio/src/test/java/com/holamundo/mimicroservicio/MiMicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holamundo.mimicroservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiMicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
