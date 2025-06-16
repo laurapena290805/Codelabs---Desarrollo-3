a438c821a62355c444d22c7c31ba5c027466db51
laurapena290805 <penaloza.laura@correounivalle.edu.co>
2025-06-16 12:01:49 -0500

    Comunicación entre microservicios: Sincrónica

---
+++ Comunicación Sincrónica/eureka/src/main/java/com/sincronica/eureka/EurekaApplication.java
@@ -0,0 +1,12 @@
+package com.sincronica.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
