ee043f3ffe1d1f1a434155caada4b0dead5ecb36
laurapena290805 <penaloza.laura@correounivalle.edu.co>
2025-06-16 17:14:24 -0500

    Virtual Threads en Spring Boot

---
+++ Virtual Threads/virtualthreads/src/test/java/com/example/virtualthreadsdemo/VirtualthreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.virtualthreadsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualthreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
